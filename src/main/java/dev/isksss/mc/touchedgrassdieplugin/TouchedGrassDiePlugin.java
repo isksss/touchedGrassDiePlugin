94d689e28acb9dfbd2109c1317003413f436edff
isksss <104404522+isksss@users.noreply.github.com>
2023-03-26 23:17:42 +0900

    wip: 草ブロックに触れたかチェックするとこまで

--- src/main/java/dev/isksss/mc/touchedgrassdieplugin/TouchedGrassDiePlugin.java
+++ src/main/java/dev/isksss/mc/touchedgrassdieplugin/TouchedGrassDiePlugin.java
@@ -1,8 +1,15 @@
 package dev.isksss.mc.touchedgrassdieplugin;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TouchedGrassDiePlugin extends JavaPlugin {
+public final class TouchedGrassDiePlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -13,5 +20,29 @@ public final class TouchedGrassDiePlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onTouchGrass(PlayerMoveEvent e){
+        Player player = e.getPlayer();
+        Location playerLoc = player.getLocation();
+
+
+        //一個下のブロック
+        int blockLocX =playerLoc.getBlockX();
+        int blockLocY =playerLoc.getBlockY() - 1;
+        int blockLocZ =playerLoc.getBlockZ();
+
+        Location blockLoc = null;
+        blockLoc.set(blockLocX,blockLocY,blockLocZ);
+
+        Block block = blockLoc.getBlock();
+        Material material = block.getBlockData().getMaterial();
+
+        //todo: 草ブロックにふれたプレイヤーをキル
+        if (material == Material.GRASS_BLOCK || material == Material.GRASS){
+            player.setKiller(player);
+            e.setCancelled(true);
+        }
     }
 }
