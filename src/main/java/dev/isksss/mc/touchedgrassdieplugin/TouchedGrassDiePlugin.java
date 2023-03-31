93dcdfcdad56e1dfcbd4f3c260604508b6a34065
isksss <104404522+isksss@users.noreply.github.com>
2023-04-01 00:53:57 +0900

    修正とActions追加

--- src/main/java/dev/isksss/mc/touchedgrassdieplugin/TouchedGrassDiePlugin.java
+++ src/main/java/dev/isksss/mc/touchedgrassdieplugin/TouchedGrassDiePlugin.java
@@ -2,11 +2,13 @@ package dev.isksss.mc.touchedgrassdieplugin;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TouchedGrassDiePlugin extends JavaPlugin implements Listener {
@@ -14,12 +16,14 @@ public final class TouchedGrassDiePlugin extends JavaPlugin implements Listener 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        // イベントを解除
+        PlayerMoveEvent.getHandlerList().unregister((Plugin) this);
     }
 
     @EventHandler
@@ -40,8 +44,13 @@ public final class TouchedGrassDiePlugin extends JavaPlugin implements Listener 
         Material material = block.getBlockData().getMaterial();
 
         //todo: 草ブロックにふれたプレイヤーをキル
-        if (material == Material.GRASS_BLOCK || material == Material.GRASS){
+        if (material == Material.GRASS_BLOCK || material == Material.GRASS) {
             player.setKiller(player);
+            World world = e.getTo().getWorld();
+            String msg = player.getName() + "は草ブロックを踏んでしまった！！";
+
+            world.getPlayers().forEach(p -> p.sendMessage(msg));
+            player.setHealth(0);
             e.setCancelled(true);
         }
     }
